093aced1d314a77230c5f91598b074d8a714df24
Aletvia Lecona <aletvialecona@gmail.com>
2021-01-14 13:43:22 -0600

    CRUD users

--- UsersManagement/forms.py
+++ UsersManagement/forms.py
@@ -8,3 +8,9 @@ class UserForm( UserCreationForm ):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+
+class ChangeUserForm( forms.ModelForm ):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
--- UsersManagement/views.py
+++ UsersManagement/views.py
@@ -1,23 +1,34 @@
-from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from UsersManagement.forms import UserForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from UsersManagement.forms import UserForm, ChangeUserForm
 
-class Users(View):
+class Users(LoginRequiredMixin, View):
+    login_url = 'login'
+    redirect_field_name = 'redirect_to'
+
     def get(self, request):
+        users = User.objects.filter(is_staff=False)
         return render(request, 'users/index.html', {
-            'title' : 'Usuarios'
+            'title' : 'Usuarios',
+            'users' : users
         })
 
-class CreateUser(View):
+
+class CreateUser(LoginRequiredMixin, View):
+    login_url = 'login'
+    redirect_field_name = 'redirect_to'
+
     def get(self,request):
-        register_form = UserForm()
 
         return render( request, 'users/user_form.html', {
-            'title' : 'Registrar usuario',
-            'register_form' : register_form
+            'title' : 'Registrar usuario'
         })
+
     def post(self, request):
         print('POST entra')
         register_form = UserForm( request.POST )
@@ -30,3 +41,49 @@ class CreateUser(View):
         else:
             return redirect('create_users')
 
+
+class UpdateUser(LoginRequiredMixin, View):
+    login_url = 'login'
+    redirect_field_name = 'redirect_to'
+
+    def get(self, request, id):
+        user_u = User.objects.get(id=id)
+
+        return render( request, 'users/user_form.html', {
+            'title' : 'Editar usuario',
+            'user_u' : user_u
+        })
+
+    def post(self, request, id):
+        user_u = User.objects.get(id=id)
+        form = ChangeUserForm(request.POST,instance=user_u)
+        print('///////////////////////////////////Actualizando usuario///////////////////////////////////')
+        if form.is_valid():
+            form.cleaned_data
+            form.save()
+            messages.success(request, 'El usuario se ha actualizado con éxito')
+            return redirect('users_list')
+        else:
+            print(form.errors)
+            form = UserForm()
+            messages.error(request, 'UPS ... existe un problema con alguno de los campos. Por favor verifica y vuelve a intentarlo.')
+            return render( request, 'users/user_form.html', {
+                'title' : 'Editar usuario',
+                'user_u' : user_u
+            })
+
+
+class DeleteUser(LoginRequiredMixin, View):
+    login_url = 'login'
+    redirect_field_name = 'redirect_to'
+
+    def post(self, request, id):
+        usr = User.objects.get(id=id)
+        usr.delete()
+        messages.success(request, 'El usuario se ha eliminado con éxito')
+        return redirect('users_list')
+
+
+def LogoutUser(request):
+    logout(request)
+    return redirect('login')
